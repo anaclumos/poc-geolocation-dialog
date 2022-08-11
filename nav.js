892e2de0086de86e8212f2f6fa0155072c239416
Sunghyun Cho <mail@chosunghyun.com>
2022-08-11 21:29:21 +0900

    finish poc

--- nav.js
+++ nav.js
@@ -1,46 +1,57 @@
-class navigator {
-  constructor(navigator) {
-    this.navigator = navigator
-  }
-  get geolocation() {
-    getCurrentPosition(success, error)
-    {
-      const dialog = this.document.createElement('dialog')
-      dialog.innerHTML = html` <div>
-        <h1>I am a pretty permission dialog.</h1>
-
+const navigator = {
+  allowed: false,
+  geolocation: {
+    getCurrentPosition(success) {
+      const dialog = document.createElement('dialog')
+      dialog.innerHTML = `
+      <div>
         <form method="dialog">
           <section>
-            <p>
-              <label for="favAnimal">Favorite animal:</label>
-              <select id="favAnimal" name="favAnimal">
-                <option></option>
-                <option>Brine shrimp</option>
-                <option>Red panda</option>
-                <option>Spider monkey</option>
-              </select>
-            </p>
+            <h1>Hey! Please allow location 😽 uwu</h1>
+            <iframe
+              src="https://giphy.com/embed/MDJ9IbxxvDUQM"
+              width="480"
+              height="270"
+              frameborder="0"
+              class="giphy-embed"
+              allowfullscreen
+            ></iframe>
           </section>
           <menu>
-            <button id="cancel" type="reset">Cancel</button>
-            <button type="submit">Confirm</button>
+            <button id="cancel" type="reset">NO</button>
+            <button id="submit" type="submit">OK</button>
           </menu>
         </form>
-
-        <iframe
-          src="https://giphy.com/embed/MDJ9IbxxvDUQM"
-          width="480"
-          height="270"
-          frameborder="0"
-          class="giphy-embed"
-          allowfullscreen
-        ></iframe>
-        <form method="dialog">
-          <button>OK</button>
-        </form>
-      </div>`
-      dialog.setAttribute('open', true)
-      this.navigator.geolocation.getCurrentPosition(success, error)
-    }
-  }
+      </div>
+      `
+      document.body.appendChild(dialog)
+      const cancelButton = document.getElementById('cancel')
+      cancelButton.addEventListener('click', () => {
+        alert('You have denied location access.')
+        document.getElementById('location').innerText = 'Denied'
+        dialog.close()
+      })
+      const submitButton = document.getElementById('submit')
+      submitButton.addEventListener('click', () => {
+        navigator.allowed = true
+        dialog.close()
+        return success({
+          coords: {
+            latitude: 77,
+            longitude: 77,
+          },
+        })
+      })
+      if (!navigator.allowed) {
+        dialog.showModal()
+      } else {
+        return success({
+          coords: {
+            latitude: 77,
+            longitude: 77,
+          },
+        })
+      }
+    },
+  },
 }
